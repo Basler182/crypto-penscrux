16c90f0ebfa2fd2b2a43f8945806acb80e5d3b84
Basler182 <showdialog@web.de>
2025-11-26 22:57:54 +0100

    add main application class and initial test for context loading

---
+++ src/main/java/com/basler182/cryptopenscrux/CryptoPenscruxApplication.java
@@ -0,0 +1,12 @@
+package com.basler182.cryptopenscrux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoPenscruxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoPenscruxApplication.class, args);
+    }
+}
---
+++ src/test/java/com/basler182/cryptopenscrux/CryptoPenscruxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basler182.cryptopenscrux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoPenscruxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
